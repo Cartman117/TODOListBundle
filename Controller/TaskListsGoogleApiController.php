159c36492b54cfc6062bdff57d035e734025451c
Pignot <kevin.pignot@live.fr>
2014-06-09 17:46:46 +0200

    Possible to add a tasklist now

--- Controller/TaskListsGoogleApiController.php
+++ Controller/TaskListsGoogleApiController.php
@@ -23,11 +23,33 @@ class TaskListsGoogleApiController extends Controller implements TaskListsInterf
         $googleClient->setAccessToken($token->getUser());
         $service = new  \Google_Service_Tasks($googleClient);
         $taskLists = $service->tasklists->listTasklists();
+
         return $this->render('TODOListBundle:TaskListsGoogleApi:index.html.twig', ['taskLists' => $taskLists]);
     }
 
     public function newTaskListAction(Request $request)
     {
-        // TODO: Implement newTaskListAction() method.
+        $client = $this->container->get("happyr.google.api.client");
+        $googleClient = $client->getGoogleClient();
+        $token = $_SESSION['token'];
+        $googleClient->setAccessToken($token->getUser());
+        $service = new \Google_Service_Tasks($googleClient);
+
+        $taskList = new \Google_Service_Tasks_TaskList();
+
+        $form = $this->createFormBuilder()
+            ->add("title")
+            ->add("Créer", "submit")
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $taskList->setTitle($form->getData()['title']);
+            $service->tasklists->insert($taskList);
+
+            return $this->redirect($this->generateUrl("todolist_googleapi_list_taskslist"));
+        }
+
+        return $this->render('TODOListBundle:TaskLists:newTaskListForm.html.twig', ['form' => $form->createView()]);
     }
 }
